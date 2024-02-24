79749c9ada4f3a688e0b638c71e7248772d0917f
Yousef Noor <yous3fnoor@gmail.com>
2024-02-24 16:29:04 -0600

    Lil Update

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,21 @@
-import { useState } from "react";
-import Alert from "./components/Alert";
-import Button from "./components/Button";
+// import { useState } from "react";
+// import Alert from "./components/Alert";
+// import Button from "./components/Button";
+import Header from "./components/Header";
+import TransactionItem from "./components/TransactionItem";
 
 function App() {
-  const [alertVisible, setAlertVisibility] = useState(false);
+  // const [alertVisible, setAlertVisibility] = useState(false);
 
   return (
     <div>
-      {alertVisible && <Alert onClose={() => setAlertVisibility(false)}>My alert</Alert>}
-      <Button color="primary" onClick={() => setAlertVisibility(true)}>Button</Button>
+      <Header />
+      <TransactionItem transaction={{id: "1", amount: "100", description: 50, date: new Date()}} />
     </div>
+    // <div>
+    //   {alertVisible && <Alert onClose={() => setAlertVisibility(false)}>My alert</Alert>}
+    //   <Button color="primary" onClick={() => setAlertVisibility(true)}>Button</Button>
+    // </div>
   );
 }
 
---
+++ src/components/Header.tsx
@@ -0,0 +1,9 @@
+const Header = () => {
+  return (
+    <header>
+        <h1>PayMe</h1>
+    </header>
+  )
+}
+
+export default Header
